fc4ab4f89e71b1f274571fec7afd34b9b2d82e10
YoungKim164 <123128919+YoungKim164@users.noreply.github.com>
2023-02-22 02:24:35 -0500

    Add files via upload

--- scripts/figure 1.R
+++ scripts/figure 1.R
@@ -12,6 +12,8 @@ fig1_data %>%
     title = "Trend in US Birth Rates",
     x = "Years",
     y = "Births per 1,000 women age 15-44",
-    caption = "Source: Birth Rates collected from CDC Vital Statistics Births Reports for 2015, 2019 and 2020."
-  )  +
+    caption = "Source: Birth Rates collected from CDC Vital Statistics Births Reports for 2015, 2019 and 2020.") +
+  geom_vline(xintercept = 2007,
+             linetype = "dashed") +
+  geom_text(aes(x = 2007, y = 70), nudge_x = 2, nudge_y = 1, label = "2007") +
   theme_light()
