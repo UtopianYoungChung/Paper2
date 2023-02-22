ae54ac1de513a4023c9a01519ebc5ac0a4cf449c
YoungKim164 <123128919+YoungKim164@users.noreply.github.com>
2023-02-22 14:31:25 -0500

    Add files via upload

--- scripts/figure 5.R
+++ scripts/figure 5.R
@@ -36,12 +36,12 @@ fig5_data <- fig5_data %>%
 fig5_data <- fig5_data %>% 
   group_by(age_2024_year) %>% 
   mutate(cum_birthrate = cumsum(birthrate)/1000, 
-         cohort = case_when(cohort2 == 1 ~ "1968-1972",
-                                  cohort2 == 2 ~ "1973-1977",
-                                  cohort2 == 3 ~ "1978-1982",
-                                  cohort2 == 4 ~ "1983-1987",
-                                  cohort2 == 5 ~ "1988-1992",
-                                  cohort2 == 6 ~ "1993-1997"))
+         cohort = case_when(cohort2 == 1 ~ "1968-1972        1992",
+                            cohort2 == 2 ~ "1973-1977        1997",
+                            cohort2 == 3 ~ "1978-1982        2002",
+                            cohort2 == 4 ~ "1983-1987        2007",
+                            cohort2 == 5 ~ "1988-1992        2012",
+                            cohort2 == 6 ~ "1993-1997        2017"))
 
 fig5_data <-  na.omit(fig5_data)
 fig5_data %>%
@@ -51,11 +51,12 @@ fig5_data %>%
   geom_line()  +
   labs(
     title = "Children Ever Born by Mother’s Age and Birth Cohort",
-    x = "ages",
-    y = "cumulative birthrate",
-    caption = "Source: Authors’ calculations based on NCHS Vital Statistics birth microdata and CDC SEER population
-data. See the Data Appendix for specific details.",
-    color = "Birth cohorts"
+    x = "Ages",
+    y = "Cumulative birthrate",
+    caption = "Source: NCHS Vital Statistics birth microdata and CDC SEER population data.",
+    color = "Birth cohorts   Age 20 in"
   ) +
-  scale_x_continuous(limits=c(15, 44), breaks=seq(15, 44, by = 2)) +
-  theme_light()
+  scale_x_continuous(limits=c(15, 44), breaks=seq(15, 44, by = 3)) +
+  theme_minimal()+
+  theme(legend.position = "right")
+
