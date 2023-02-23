ce587ad0d4436f71666bef0cd6a06bfddc35a25c
Joseph Chung <122658908+UtopianYoungChung@users.noreply.github.com>
2023-02-23 13:13:19 -0500

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -14,22 +14,23 @@
 library(haven)
 library(tidyverse)
 
+# Reading in the data
+fig1_data = read.csv(here::here("inputs/data/fig_1.csv"))
+agecomp_data = read_dta(here::here("inputs/data/pop/agecomp-seer.dta"))
+birth1 = read_dta(here::here("inputs/data/nchs/nchs_births_pop_1990_2019.dta"))
+pop1 = read_dta(here::here("inputs/data/pop/age_race_comp_seer.dta"))
+rentcost_0419 = read_dta(here::here("inputs/data/long_term/longdiff-rentcost-0419.dta"))
+rhs_1519 = read_dta(here::here("inputs/data/long_term/longdiff-RHS-1519.dta"))
+rhs_0408 = read_dta(here::here("inputs/data/long_term/longdiff-RHS-0408.dta"))
+std_loan = read_dta(here::here("inputs/data/long_term/nyfed-student-debt-per-capita-0419.dta"))
 
-
-#### Workspace setup ####
-library(gridExtra)
-library(cdlTools)
-library(RGraphics)
-library(tidyverse)
-library(palmerpenguins)
-library(knitr)
-library(ggplot2)
-library(haven)
-library(dplyr)
-options(dplyr.summarise.inform = FALSE)
-
-#### Simulate data ####
-fig1_data <- read.csv(here::here("inputs/data/fig_1.csv"))
-
-
+# Create datasets from dta files
+write_csv(fig1_data, "Inputs/Data/fig1_data.csv")
+write_csv(agecomp_data, "Inputs/Data/agecomp_data.csv")
+write_csv(birth1, "Inputs/Data/birth1.csv")
+write_csv(pop1, "Inputs/Data/pop1.csv")
+write_csv(rentcost_0419, "Inputs/Data/rentcost_0419.csv")
+write_csv(rhs_1519, "Inputs/Data/rhs_1519.csv")
+write_csv(rhs_0408, "Inputs/Data/rhs_0408.csv")
+write_csv(std_loan, "Inputs/Data/std_loan.csv")
 
