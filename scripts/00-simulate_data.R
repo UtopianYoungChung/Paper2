c4659cd5429db8a4e5ab5dfa5d4d6d93ab1de028
Joseph Chung <122658908+UtopianYoungChung@users.noreply.github.com>
2023-02-23 13:25:32 -0500

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -15,7 +15,6 @@ library(haven)
 library(tidyverse)
 
 # Reading in the data
-fig1_data = read.csv(here::here("inputs/data/fig_1.csv"))
 agecomp_data = read_dta(here::here("inputs/data/pop/agecomp-seer.dta"))
 birth1 = read_dta(here::here("inputs/data/nchs/nchs_births_pop_1990_2019.dta"))
 pop1 = read_dta(here::here("inputs/data/pop/age_race_comp_seer.dta"))
@@ -25,12 +24,11 @@ rhs_0408 = read_dta(here::here("inputs/data/long_term/longdiff-RHS-0408.dta"))
 std_loan = read_dta(here::here("inputs/data/long_term/nyfed-student-debt-per-capita-0419.dta"))
 
 # Create datasets from dta files
-write_csv(fig1_data, "Inputs/Data/fig1_data.csv")
-write_csv(agecomp_data, "Inputs/Data/agecomp_data.csv")
-write_csv(birth1, "Inputs/Data/birth1.csv")
-write_csv(pop1, "Inputs/Data/pop1.csv")
-write_csv(rentcost_0419, "Inputs/Data/rentcost_0419.csv")
-write_csv(rhs_1519, "Inputs/Data/rhs_1519.csv")
-write_csv(rhs_0408, "Inputs/Data/rhs_0408.csv")
-write_csv(std_loan, "Inputs/Data/std_loan.csv")
+write_csv(x = agecomp_data, file = here::here("Inputs/Data/agecomp_data.csv"))
+write_csv(x = birth1, file = here::here("Inputs/Data/birth1.csv"))
+write_csv(x = pop1, file = here::here("Inputs/Data/pop1.csv"))
+write_csv(x = rentcost_0419, file = here::here("Inputs/Data/rentcost_0419.csv"))
+write_csv(x = rhs_1519, file = here::here("Inputs/Data/rhs_1519.csv"))
+write_csv(x = rhs_0408, file = here::here("Inputs/Data/rhs_0408.csv"))
+write_csv(x = std_loan, file = here::here("Inputs/Data/std_loan.csv"))
 
